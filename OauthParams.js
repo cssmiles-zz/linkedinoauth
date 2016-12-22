f75ab77a5cb8f656174e8492163c9491896cb429
csp <cssmiles@gmail.com>
2016-12-22 14:40:48 -0500

    initial commit

--- OauthParams.js
+++ OauthParams.js
@@ -1,6 +1,6 @@
 const OauthParams = {
-    client_id: "77arqe48ycj44b",
-    client_secret: "YSZitdSRMA5Vpe9O",
-    redirect_uri: "http://localhost:5000/auth"
+    client_id: "",
+    client_secret: "",
+    redirect_uri: ""
 }
 module.exports = OauthParams;
